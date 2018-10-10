46b65278e7e9f80ea6547c64cd2f7d610eaec3f7
Christian Lamb <christianlamb@MacBook-Pro-52.local>
2018-10-10 08:43:11 -0500

    Complete  Ui

---
+++ Ios Guessing Game/Difficulty .swift
@@ -0,0 +1,20 @@
+//
+//  Difficulty .swift
+//  Ios Guessing Game
+//
+//  Created by Christian Lamb on 10/10/18.
+//  Copyright © 2018 Christian Lamb. All rights reserved.
+//
+
+import UIKit
+
+
+
+class Difficulty : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
+
